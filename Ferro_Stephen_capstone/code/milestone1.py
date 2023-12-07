31e955c7bdb3b852370424e2de9b3a5a235b3afb
Stephen <stephencferro@gmail.com>
2023-12-06 19:11:03 -0600

    added test cases and write_csv function

--- Ferro_Stephen_capstone/code/milestone1.py
+++ Ferro_Stephen_capstone/code/milestone1.py
@@ -6,7 +6,7 @@ def NextState(currentConfig, currentSpeeeds, dt=0.01, maxAngSpeed=2*np.pi):
 
     l =             # the distance from the center of the robot to the front/rear axle (see p.521 of Modern Robotics)
     w =             # the distance from the center of the robot to the LH/RH wheel centerline (see p.521 of Modern Robotics)
-    r =             # the wheel radius
+    r =             # the wheel radii
 
     for speed in currentSpeeeds:
         if speed > maxAngSpeed:
@@ -25,10 +25,43 @@ def NextState(currentConfig, currentSpeeeds, dt=0.01, maxAngSpeed=2*np.pi):
     
     chassisVel = r * np.matmul(dimensionArrayInv, u)
 
-    newConfig = [chassisVel[0], chassisVel[1], chassisVel[2],                                                                                                                               # chassis state: phi, x, y 
-                 currentConfig[3]+angleChange[0], currentConfig[4]+angleChange[1], currentConfig[5]+angleChange[2], currentConfig[6]+angleChange[3], currentConfig[7]+angleChange[4],       # arm joint positions: theta1, theta2, theta3, theta4, theta5
-                 currentConfig[8]+angleChange[5], currentConfig[9]+angleChange[6], currentConfig[10]+angleChange[7], currentConfig[11]+angleChange[8]]                                      # wheel positions: wheel1, wheel2, wheel3, wheel4
+    newConfig = np.array([chassisVel[0], chassisVel[1], chassisVel[2],                                                                                                                              # chassis state: phi, x, y 
+                    currentConfig[3]+angleChange[0], currentConfig[4]+angleChange[1], currentConfig[5]+angleChange[2], currentConfig[6]+angleChange[3], currentConfig[7]+angleChange[4],            # arm joint positions: theta1, theta2, theta3, theta4, theta5
+                    currentConfig[8]+angleChange[5], currentConfig[9]+angleChange[6], currentConfig[10]+angleChange[7], currentConfig[11]+angleChange[8]])                                          # wheel positions: wheel1, wheel2, wheel3, wheel4
     
     return newConfig
 
+
+def write_to_csv(array, file_name):
+    """
+    Decription:
+        Writes an array to a csv file
+    Inputs:
+        • array: The array to be written to the file
+        • file_name: The filename to be used
+    """
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(array)
+
+
+u1 = np.array([0, 0, 0, 0, 0, 10, 10, 10, 10])
+u2 = np.array([0, 0, 0, 0, 0, -10, 10, -10, 10])
+u3 = np.array([0, 0, 0, 0, 0, -10, 10, 10 , -10])
+
+config = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+time = 0.0
+dt = 0.01
+
+filename = 'milestone1.csv'
+configArray = np.array([config])
+
+while time <= 1.0:
+    config = NextState(config, u1, dt)
+    configArray = np.vstack(configArray, config)
+    time += dt
+
+print(configArray)
+write_to_csv(configArray, filename)
 
